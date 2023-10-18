205650da065d66a1dbf6f51da03a521546554c64
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2023-10-18 15:42:37 +0200

    Make package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neural_sampling_code",
+    version="0.0.0",
+    description="Code for the paper Shrinivasan et al. NeurIPS 2023",
+    author="Suhas Shrinivasan",
+    author_email="suhasshrinivasan@gmail.com",
+    packages=find_packages(),
+)
